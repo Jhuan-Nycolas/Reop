2884c19b3f5b58e658ff67ba075d3df77eedd3e8
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-04-09 10:04:56 -0300

    Build pronta

--- default.nix
+++ default.nix
@@ -5,15 +5,12 @@
 stdenv.mkDerivation {
   name = "reop";
 
-  src = ./src;
+  src = ./dist;
 
   buildInputs = [python3];
 
   installPhase = ''
     mkdir -p $out/bin
-    cp -r $src/* $out/bin/
-    echo '#!/usr/bin/env python3' > $out/bin/reop
-    echo 'exec(open("'"$out/bin/open.py"'").read())' >> $out/bin/reop
-    chmod +x $out/bin/reop
+    cp -r $src/reop $out/bin/
   '';
 }
